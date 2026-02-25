737fa151da7c6357872fb40fdbf89d78b98d3765
Arjun R <arjunr0019@gmail.com>
2026-02-26 02:04:07 +0530

    fine adjustment to request window

--- src/App.jsx
+++ src/App.jsx
@@ -190,7 +190,7 @@ function AppContent() {
 
       <Box sx={{ flexGrow: 1, p: 0, overflow: 'hidden', '& .split-flex': { display: 'flex', height: '100%' } }}>
         <Split
-          sizes={[20, 55, 25]}
+          sizes={[18, 52, 30]}
           minSize={[200, 300, 300]}
           expandToMin={false}
           gutterSize={4}
